ac41702b6a55232170c6a7bb379c05cb98ba3078
Mikhail Shevelev <mikhail.shevelev@ideasoft.io>
2023-01-06 12:00:05 +0300

    141-Linked-List-Cycle

---
+++ 141-Linked-List-Cycle/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println()
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
