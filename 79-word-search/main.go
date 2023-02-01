00393e36a3787e6ae5ca1047add87bf07a7af794
Mikhail Shevelev <mishka2017@gmail.com>
2023-02-01 14:04:22 +0800

    79-word-search

---
+++ 79-word-search/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCESEEEFS"))
+	// fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "SEE"))
+	// fmt.Println(exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"))
+}
+
+func exist(board [][]byte, word string) (contains bool) {
+	if word == "" {
+		return true
+	}
+
+	for y := 0; y < len(board); y++ {
+		for x := 0; x < len(board[y]); x++ {
+			if word[0] == board[y][x] {
+				var visited [][]bool
+				for i := range board {
+					visited = append(visited, make([]bool, len(board[i])))
+				}
+				contains = search(board, word, x, y, visited)
+				if contains {
+					return
+				}
+			}
+		}
+	}
+	return
+}
+
+func search(board [][]byte, word string, x, y int, visited [][]bool) bool {
+	if x < 0 || y < 0 || y >= len(board) || x >= len(board[0]) {
+		return false
+	}
+	if visited[y][x] {
+		return false
+	}
+	if len(word) == 1 && word[0] == board[y][x] {
+		return true
+	} else if len(word) == 1 {
+		return false
+	} else if word[0] == board[y][x] {
+		visited[y][x] = true
+		defer func() {
+			visited[y][x] = false
+		}()
+		t := search(board, word[1:], x, y+1, visited)
+		b := search(board, word[1:], x, y-1, visited)
+		l := search(board, word[1:], x-1, y, visited)
+		r := search(board, word[1:], x+1, y, visited)
+		if t || b || l || r {
+			return true
+		}
+	}
+	return false
+}
