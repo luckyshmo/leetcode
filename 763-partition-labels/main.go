be6055ed71748977c717d6a43efbd2577ebde6e8
Mikhail Shevelev <mishka2017@gmail.com>
2023-05-23 18:08:50 +0200

    763-partition-labels

--- 763-partition-labels/main.go
+++ 763-partition-labels/main.go
@@ -8,15 +8,15 @@ func main() {
 	fmt.Println()
 }
 
-func partitionLabels(S string) []int {
+func partitionLabels(s string) []int {
 	last := make(map[rune]int)
-	for i, c := range S {
+	for i, c := range s {
 		last[c] = i
 	}
 	j := 0
 	anchor := 0
 	ans := []int{}
-	for i, c := range S {
+	for i, c := range s {
 		j = max(j, last[c])
 		if i == j {
 			ans = append(ans, i-anchor+1)
